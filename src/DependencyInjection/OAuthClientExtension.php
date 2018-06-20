52ccdf990045db2796e4aed98af4cf415f54eb5a
Nathan <nathan@2le.net>
2018-06-20 17:04:02 +0200

    Changed configuration

--- src/DependencyInjection/OAuthClientExtension.php
+++ src/DependencyInjection/OAuthClientExtension.php
@@ -4,21 +4,22 @@ namespace Lle\OAuthClientBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader;
 
 class OAuthClientExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        $config = $this->processConfiguration(new Configuration(), $configs);
-        $container->setParameter('lle_oauth_client.config', $config);
-    }
+        // read yaml from knpu oauth2 client
+        // $config_path = __DIR__ . '/../Resources/config/knpu_oauth2_client_config.yaml';
+        // $yaml = (new Parser())->parse(file_get_contents($config_path));
+        // put it in symfony
 
-    /**
-     * We don't want Symfony to call it o_auth_client.
-     * @return string
-     */
-    public function getAlias()
-    {
-        return "lle_oauth_client";
+        // SERVICES
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
--- src/OAuthClientBundle.php
+++ src/OAuthClientBundle.php
@@ -7,16 +7,5 @@ use Lle\OAuthClientBundle\DependencyInjection\OAuthClientExtension;
 
 class OAuthClientBundle extends Bundle
 {
-    /**
-     * So the alias isn't o_auth_client in the config files.
-     * @return ExtensionInterface
-     */
-    public function getContainerExtension()
-    {
-        if ($this->extension === null) {
-            return new OAuthClientExtension();
-        }
-
-        return $this->extension;
-    }
+    
 }
