1a888fb4b09f0ac39e295b02637f16ab48774268
valentin-helies <89925635+valentin-helies@users.noreply.github.com>
2024-06-10 16:11:50 +0200

    Update UserProvider.php

--- src/Security/User/UserProvider.php
+++ src/Security/User/UserProvider.php
@@ -55,7 +55,7 @@ class UserProvider implements UserProviderInterface
         return $user;
     }
 
-    public function refreshUser(UserInterface $user)
+    public function refreshUser(UserInterface $user): UserInterface
     {
         if (!$user instanceof User) {
             throw new UnsupportedUserException(
