bd56f76b8b3fe58d5a98b14a591acbfd530dfc78
Quentin HAETTEL <55276836+QuentinHAETTEL@users.noreply.github.com>
2025-09-15 09:57:16 +0200

    Compatibilité PHP 8.1

--- src/Exception/ConnectException.php
+++ src/Exception/ConnectException.php
@@ -5,13 +5,13 @@ namespace Lle\OAuthClientBundle\Exception;
 class ConnectException extends \Exception
 {
     // default
-    public const int UNKNOWN_ERROR = 0;
+    public const UNKNOWN_ERROR = 0;
     // requested resource does not exist
-    public const int NOT_FOUND = 1;
+    public const NOT_FOUND = 1;
     // username or email already taken
-    public const int USER_ALREADY_EXISTS = 2;
+    public const USER_ALREADY_EXISTS = 2;
     // validation (asserts) failed; see content
-    public const int VALIDATION_FAILED = 3;
+    public const VALIDATION_FAILED = 3;
 
     protected array $data = [];
 
